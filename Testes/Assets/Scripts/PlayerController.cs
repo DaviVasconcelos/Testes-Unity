be9bba801f3c5a5cd1d7880fcd6bce0bded63440
Davi Vasconcelos <pd350854@gmail.com>
2025-04-28 19:37:56 -0300

    colisão consertada com novo método de movimentação

--- Testes/Assets/Scripts/PlayerController.cs
+++ Testes/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
+// Garante que o script usará o Rigidbody2D
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
@@ -9,11 +12,17 @@ public class PlayerController : MonoBehaviour
     public float speed = 2.5f;
     bool isWalking = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rig2d;
+    // Vetor de 2 lados para o movimento, começando vazio com o .zero
+    Vector2 movement = Vector2.zero;
+
     void Start()
     {
-        // Quando o jogo iniciar, a anmimação idle começa
+        // Quando o jogo iniciar, a animação idle começa
         isWalking = false;
+
+        // atualiza o rig2d com base no rigidbody2d da unity 
+        rig2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,12 +39,11 @@ public class PlayerController : MonoBehaviour
             isWalking = true;
         }
 
+        // Mecânica de movimentação
+        movement = new Vector2(input_x, input_y);
+
         if (isWalking)
         {
-            var move = new Vector3 (input_x, input_y, 0).normalized;
-
-            transform.position += move * speed * Time.deltaTime;
-
             // Animação
             playerAnimator.SetFloat("input_x", input_x);
             playerAnimator.SetFloat("input_y", input_y);
@@ -47,5 +55,11 @@ public class PlayerController : MonoBehaviour
         {
             playerAnimator.SetTrigger("attack");
         }
+    }
+
+    private void FixedUpdate()
+    {
+        // rig2d.position é a posição atual do player, o + movement é para somar o input, assim movendo
+        rig2d.MovePosition(rig2d.position + movement * speed * Time.fixedDeltaTime);
     }
 }
